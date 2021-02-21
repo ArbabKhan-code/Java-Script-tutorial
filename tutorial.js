2147fbdf3beb4655a40ac24736b43480c999954d
Arbab Khan <ak.arbabkhan4@gmail.com>
2021-02-21 14:21:23 +0530

    practising condition statments

--- tutorial.js
+++ tutorial.js
@@ -1,3 +1,37 @@
 document.write("<h1>Hello world!</h1>");
-var data = "Kyo aaya hai ess sight par";
-alert(data);
+/*var num = 11;
+if(num>0 && num%2==0)
+{
+    document.write('number is eben and positive');
+}
+else {
+    document.write("not aligible");
+}*/
+//-----------------------------------------------------------------------//
+/*var day =10;
+switch(day){
+    case 1:
+        document.write("Monday");
+        break;
+    case 2:
+        document.write("Tuesday");
+        break;
+    case 3:
+        document.write("wednesday");
+        break;
+    case 4:
+        document.write("Thrusday");
+    case 5:
+        document.write("Friday");
+        break;
+    case 6:
+        document.write("Saturday");
+        break;
+    case 7:
+        document.write("Sunday");
+        break;
+
+default: document.write("wrong input")
+}*/
+
+//----------------------------------------------------------------------------------//
